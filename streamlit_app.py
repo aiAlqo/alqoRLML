7d53ffd9bff823691c14e40525581ed8d99e61c2
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-14 19:03:44 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🏉 RL Machine Learning App')
 
 st.info('This is app builds a machine learning model')
+
+df = pd.read_csvd'https://www.kaggle.com/datasets/thecruncherau/nrl-results?resource=download'
+df
