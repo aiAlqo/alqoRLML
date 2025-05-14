c16c751a761255b2c6e6bb85edb7d8a292b3af9f
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-14 19:11:45 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🏉 RL Machine Learning App')
 
 st.info('This is app builds a machine learning model')
 
-df = pd.read_csv('C:\1 Coding Projects\Machine Learning\NRL\Dataset\nrldata_2017to21.csv')
+df = pd.read_csv(r'C:\1 Coding Projects\Machine Learning\NRL\Dataset\nrldata_2017to21.csv')
 df
