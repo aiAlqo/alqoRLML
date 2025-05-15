da5f5a2a96711404b90b35073490b99af2f7b5f1
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-15 13:07:57 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🏉 RL Machine Learning App')
 st.info('This is app builds a machine learning model')
 
 with st.expander('NRL Data'):
-  st.write('**Raw Data 2017 to 2021**)
+  st.write('**Raw Data 2017 to 2021**')
   df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/nrldata_2017to21.csv')
   df
