444a941f51ccd57e5e6318aac44bf36be0d6f995
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-15 19:10:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ st.info('This is app builds a machine learning model')
 
 with st.expander('NRL Data'):
   st.write('**Raw Data 2017 to 2021**')
-  df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/nrldata_2017to21.csv')
-  df
+  nrl_df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/nrldata_2017to21.csv')
+  nrl_df
 
 with st.expander('AFL Data'):
   st.write('**Raw Data 2012 to 2021**')
-  df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/AFL2012to24.csv')
-  df
+  afl_df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/AFL2012to24.csv')
+  afl_df
