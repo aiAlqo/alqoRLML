f2e894411e72d77a2ac8b1ae590c6d7df32627ff
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-19 19:16:10 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.info('This app is a machine learning prediction model for AFL and NRL')
 
 with st.expander('AFL Data'):
   st.write('**Raw Data 2012 to 2021**')
-  afl_df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/AFL2012to24_1.csv')
+  afl_df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/AFL2012to24_2.csv')
   afl_df
   #X = df.drop('HTWinner', axis=22, 'HTWinningMargin', axis=23, 'FTWinner', axis=24, 'FTWinningMargin', axis=25, 'TotalMatchPoints', axis=26, 'TotalMatchGoals', axis=27)
   cols_to_drop = ['HTWinner', 'HTWinningMargin', 'FTWinner', 'FTWinningMargin', 'TotalMatchPoints', 'TotalMatchGoals']
@@ -21,9 +21,12 @@ with st.expander('AFL Data'):
   st.dataframe(y)
 
 #Data preparations
+#List of AFL teams
+teams = ['Adelaide Crows', 'Brisbane Lions', 'Carlton', 'Collingwood', 'Essendon','Fremantle', 'Geelong Cats', 'Gold Coast Suns', 'GWS Giants', 'Hawthorn','Melbourne', 'North Melbourne', 'Port Adelaide', 'Richmond', 'St Kilda','Sydney Swans', 'West Coast Eagles', 'Western Bulldogs']
+
 with st.sidebar:
   st.header('Input Features')
   "Input match details below:"
-  HomeTeam = st.selectbox('Home Team', ('Greater Western Sydney','Richmond','Hawthorn','Melbourne','Gold Coast','Fremantle','North Melbourne','Western Bulldogs','Port Adelaide','Brisbane Lions','Essendon','Sydney','West Coast','Adelaide','Collingwood','St Kilda','Geelong','Carlton'))
-  AwayTeam = st.selectbox('Away Team', ('Greater Western Sydney','Richmond','Hawthorn','Melbourne','Gold Coast','Fremantle','North Melbourne','Western Bulldogs','Port Adelaide','Brisbane Lions','Essendon','Sydney','West Coast','Adelaide','Collingwood','St Kilda','Geelong','Carlton'))
+  HomeTeam = st.selectbox('Home Team', teams)
+  AwayTeam = st.selectbox('Away Team', teams)
 
