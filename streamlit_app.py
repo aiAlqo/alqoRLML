fbe30fd65a373c6a59fcdf79483bafc6fbe4af37
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-19 18:01:37 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,13 @@ with st.expander('AFL Data'):
   afl_df
   #X = df.drop('HTWinner', axis=22, 'HTWinningMargin', axis=23, 'FTWinner', axis=24, 'FTWinningMargin', axis=25, 'TotalMatchPoints', axis=26, 'TotalMatchGoals', axis=27)
   st.write('**X**')
-  X = df.drop('HTWinner', axis=2)
+  X = df.drop('HTWinner', axis=22)
   X
   
   st.write('**y**')
   y = df.HTWinner
   y
 
-"""
 # Sample data
 data = {
     'Item': ['Apples', 'Bananas', 'Cherries'],
@@ -60,4 +59,3 @@ else:
 # Guide preparations
 with st.sidebar:
   st.header('Tipping Guide')
-"""
