092fe752415476a2c6ca3d05b11dcd301fee2299
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-19 17:39:17 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,14 @@ with st.expander('AFL Data'):
   afl_df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/AFL2012to24.csv')
   afl_df
 
+  st.write('**X**')
+  X = df.drop('HTWinner', axis=22, 'HTWinningMargin', axis=23, 'FTWinner', axis=24, 'FTWinningMargin', axis=25, 'TotalMatchPoints', axis=26, 'TotalMatchGoals', axis=27)
+  X
+  
+  st.write('**y**')
+  y = df.HTWinner
+  y
+
 # Sample data
 data = {
     'Item': ['Apples', 'Bananas', 'Cherries'],
