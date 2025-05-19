0939cabd039af3b07e81018afc17053993eb5553
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-19 17:21:14 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🏉 RL Machine Learning App')
+st.title('🏉 AFL ML Predictor App')
 
-st.info('This is app builds a machine learning model')
-
-with st.expander('NRL Data'):
-  st.write('**Raw Data 2017 to 2021**')
-  nrl_df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/refs/heads/master/data/nrldata_2017to21.csv')
-  nrl_df
+st.info('This app is a machine learning prediction model for AFL and NRL')
 
 with st.expander('AFL Data'):
   st.write('**Raw Data 2012 to 2021**')
