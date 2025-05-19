8981b0181b543f2c6af7ba1ce6386abd0320c775
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-19 18:02:29 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,11 @@ with st.expander('AFL Data'):
   afl_df
   #X = df.drop('HTWinner', axis=22, 'HTWinningMargin', axis=23, 'FTWinner', axis=24, 'FTWinningMargin', axis=25, 'TotalMatchPoints', axis=26, 'TotalMatchGoals', axis=27)
   st.write('**X**')
-  X = df.drop('HTWinner', axis=22)
+  X = df.drop('Year', axis=1)
   X
   
   st.write('**y**')
-  y = df.HTWinner
+  y = df.Year
   y
 
 # Sample data
