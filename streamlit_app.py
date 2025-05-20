dbbd7e3394d8a614d00701267354add93c61b2ed
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-20 16:26:11 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,15 @@ st.info('This app is a machine learning prediction model for AFL and NRL')
 
 with st.expander('AFL Data'):
   st.write('**Raw Data 2012 to 2021**')
-  afl_df = pd.read_csv('https://https://raw.githubusercontent.com/aiAlqo/alqoRLML/master/data/AFL2012to24_2.csv')
-  afl_df
+  afl_df = pd.read_csv('https://raw.githubusercontent.com/aiAlqo/alqoRLML/master/data/AFL2012to24_2.csv')
+  st.dataframe(afl_df)
   #X = df.drop('HTWinner', axis=22, 'HTWinningMargin', axis=23, 'FTWinner', axis=24, 'FTWinningMargin', axis=25, 'TotalMatchPoints', axis=26, 'TotalMatchGoals', axis=27)
   cols_to_drop = ['HTWinner', 'HTWinningMargin', 'FTWinner', 'FTWinningMargin', 'TotalMatchPoints', 'TotalMatchGoals']
-  
-  st.write('**X**')
+  st.write('**Features (X):**')
   X = afl_df.drop(cols_to_drop, axis=1)
   st.dataframe(X)
   
-  st.write('**y**')
+  st.write('**Labels (y):**')
   y = afl_df[cols_to_drop]
   st.dataframe(y)
 
